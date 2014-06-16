f6f35db13643cf03ae4b8099dcfe6df6abd454bc
erroldsilva <errol@localhost.localdomain>
2014-06-16 20:35:30 +0530

    Deployment Script New Change

--- deploy/Launch.sh
+++ deploy/Launch.sh
@@ -4,6 +4,6 @@ hostip=$(echo $host | cut -d '@' -f 2)
 keyfile=$1
 #ssh-keygen -R {"$hostip"}
 #ssh-keygen -R {"$host"}
-yes | scp -i "$keyfile" BootStrap.sh "$host":~/
-ssh -i "$keyfile" "$host" -t 'sh ~/BootStrap.sh'
+scp  -o "StrictHostKeyChecking no" -i "$keyfile" BootStrap.sh "$host":~/
+ssh -o "StrictHostKeyChecking no" -i "$keyfile" "$host" -t 'sh ~/BootStrap.sh'
 
